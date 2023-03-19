092d81b1ffa7d1bc0fc34ca4dcd6ed0b553d74d5
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-03-19 19:25:21 +0530

    responsive navbar with awesome react-icons and transform using useState hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,12 +40,25 @@ function Navbar() {
           </li>
         ))}
       </ul>
+
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500"
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute w-full top-0 left-0 h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="py-6 px-4 cursor-pointer capitalize text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
